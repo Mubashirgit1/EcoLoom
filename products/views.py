6c48d03af2dc3a62df4c89dd11a02a44e170a227
Mubashirgit1 <mubashir.connect@gmail.com>
2025-11-16 14:13:05 +0000

    dispaly products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    context = {'products': products,}
+    """"View function show all products including sorting queries."""
+    return render(request, 'products/products.html',context)
