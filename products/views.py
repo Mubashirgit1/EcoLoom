73d49846558cb9258187dce8d13dfa94e1b7a842
Mubashirgit1 <mubashir.connect@gmail.com>
2026-01-04 12:07:39 +0000

    add sorting and search product filter

--- products/views.py
+++ products/views.py
@@ -11,6 +11,7 @@ def all_products(request):
     query = None
     categories = None
     sort = None
+    direction = None
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -18,6 +19,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'    
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -25,7 +28,7 @@ def all_products(request):
             products = products.order_by(sortkey)
         if 'categories' in request.GET:
             categories = request.GET['categories'].split(',')
-            products = products.filter(category__name__ini=categories)
+            products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
             
         if 'q' in request.GET:
