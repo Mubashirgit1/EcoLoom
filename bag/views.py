5e5813b8d150295447cda92c4cad4947591603f4
Mubashirgit1 <mubashir.connect@gmail.com>
2026-02-01 12:11:05 +0000

    FInished the product sizes

--- bag/views.py
+++ bag/views.py
@@ -11,10 +11,24 @@ def add_to_bag(request, item_id):
     # Implementation goes here
     quanity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size =None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+        item_id = f"{item_id}_{size}"        
     bag = request.session.get('bag', {})
-    if item_id in bag:
-        bag[item_id] += quanity
-    else:
-        bag[item_id] = quanity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quanity
+            else:
+                bag[item_id]['items_by_size'][size] = quanity
+        else:
+            bag[item_id] = {'items_by_size': {size: quanity}}
+    else:           
+        if item_id in bag:
+            bag[item_id] += quanity
+        else:
+            bag[item_id] = quanity
+            
     request.session['bag'] = bag
     return redirect(redirect_url)
