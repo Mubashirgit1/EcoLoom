68a0c583e70d6b6abcd96e5eb8ae6fb4df7fb92b
Mubashirgit1 <mubashir.connect@gmail.com>
2026-02-12 21:09:22 +0000

    stripe wenhook Test and completed

--- Ecoloom/settings.py
+++ Ecoloom/settings.py
@@ -28,7 +28,10 @@ if os.path.isfile('env.py'):
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -192,3 +195,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'GBP'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
